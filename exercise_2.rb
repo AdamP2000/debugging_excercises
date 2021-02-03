c7018f6e5e01bed54ef81b2a846c3d157a93c3ad
AdamP2000 <adam2000phillips@gmail.com>
2021-02-03 12:56:34 +0000

    Final debugging of exercise 2

--- exercise_2.rb
+++ exercise_2.rb
@@ -9,7 +9,7 @@ end
 def decode(ciphertext, key)
   cipher = key.chars.uniq + (('a'..'z').to_a - key.chars)
   plaintext_chars = ciphertext.chars.map do |char|
-    cipher[65 - char.ord]
+    cipher[char.ord - 65]
   end
   plaintext_chars.join
 end
@@ -28,4 +28,6 @@ print decode("EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL", "secretkey")
 # > decode("EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL", "secretkey")
 # => "theswiftfoxjumpedoverthelazydog"
 
-#error : nil can't be coerced into Integer (TypeError). This is because in line 3 when char is "z" it can't find the character "z" in cipher because in line 2 the array of the alphabet goes up to "z" but does not include that so first we need to change that for both methods by using 2 dots instead of 3
+#error : nil can't be coerced into Integer (TypeError). This is because in line 3 when char is "z" it can't find the character "z" in cipher because in line 2 the array of the alphabet goes up to "z" but does not include that so first we need to change that for both methods by using 2 dots instead of 3.
+
+#we are getting the expected output for the encode method but not the decode method and this is because on line 12 the subtraction to choose the index of cipher is the wrong way round.
