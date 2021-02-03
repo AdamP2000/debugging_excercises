a77b279fca80575de91cd524f5907e2be046f287
AdamP2000 <adam2000phillips@gmail.com>
2021-02-03 11:05:34 +0000

    debugging process for exercise 1

--- exercise_1.rb
+++ exercise_1.rb
@@ -2,7 +2,15 @@ def say_hello(name)
   "hello #(name)"
 end
 
- print say_hello(Adam)
+ print say_hello("Adam")
 
 
- #I expect the out output: hello Adam
+#I expect the output: hello Adam
+
+#Output I received: hello #(name)
+
+#finding the problem: line 2 of our code
+
+#fixing the problem: wrong interpolation syntax: #{name} instead of #(name)
+
+#code should now run as expected
