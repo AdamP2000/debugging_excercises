6f7c16131825c5b8ec040174239c4629d29a7aae
AdamP2000 <adam2000phillips@gmail.com>
2021-02-03 12:51:40 +0000

    First bug solved in exercise 2

--- exercise_2.rb
+++ exercise_2.rb
@@ -1,5 +1,5 @@
 def encode(plaintext, key)
-  cipher = key.chars.uniq + (('a'...'z').to_a - key.chars)
+  cipher = key.chars.uniq + (('a'..'z').to_a - key.chars)
   ciphertext_chars = plaintext.chars.map do |char|
     (65 + cipher.find_index(char)).chr
   end
@@ -7,7 +7,7 @@ def encode(plaintext, key)
 end
 
 def decode(ciphertext, key)
-  cipher = key.chars.uniq + (('a'...'z').to_a - key.chars)
+  cipher = key.chars.uniq + (('a'..'z').to_a - key.chars)
   plaintext_chars = ciphertext.chars.map do |char|
     cipher[65 - char.ord]
   end
@@ -20,7 +20,6 @@ print encode("theswiftfoxjumpedoverthelazydog", "secretkey")
 print decode("EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL", "secretkey")
 
 
-
 # Intended output:
 #
 # > encode("theswiftfoxjumpedoverthelazydog", "secretkey")
@@ -28,3 +27,5 @@ print decode("EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL", "secretkey")
 #
 # > decode("EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL", "secretkey")
 # => "theswiftfoxjumpedoverthelazydog"
+
+#error : nil can't be coerced into Integer (TypeError). This is because in line 3 when char is "z" it can't find the character "z" in cipher because in line 2 the array of the alphabet goes up to "z" but does not include that so first we need to change that for both methods by using 2 dots instead of 3
