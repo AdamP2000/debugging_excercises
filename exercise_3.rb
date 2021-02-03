37249ceafa30410c6101f00b55c6e65135c546cc
AdamP2000 <adam2000phillips@gmail.com>
2021-02-03 13:18:05 +0000

    Fixing first problem in exercise 3

--- exercise_3.rb
+++ exercise_3.rb
@@ -1,6 +1,6 @@
 def factorial(n)
   product = 1
-  while n > 0
+  while n > 1
     n -= 1
     product *= n
   end
@@ -13,3 +13,7 @@ puts factorial(5)
 #
 # > factorial(5)
 # => 120
+
+
+#Getting the output 0 and this is because of line 3 of our code. When n becomes 1 in line 4 it subtracts 1 from that and multiplies it by product so we always end up with 0.
+#We need to change the 0 to a 1 in line 3 to solve this problem
